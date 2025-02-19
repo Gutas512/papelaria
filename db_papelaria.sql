bbaa44c0e056a2ebb7fc2c885ac5e8e07805c8f6
Augusto Floresta <153240245+Gutas512@users.noreply.github.com>
2025-02-19 20:40:26 -0300

    Add files via upload

---
+++ db_papelaria.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE papelaria;
+
+USE papelaria;
+
+CREATE TABLE Livros(
+	id_livro INT AUTO_INCREMENT PRIMARY KEY,
+    liv_titulo VARCHAR(50) NOT NULL,
+    liv_ISBN INT NOT NULL,
+    liv_edicao VARCHAR(50) NOT NULL,
+    liv_editora VARCHAR(50) NOT NULL,
+    liv_ano__publicacao INT NOT NULL,
+    liv_preco_da_capa FLOAT NOT NULL,
+    liv_categoria VARCHAR(50) NOT NULL,
+    liv_quant INT NOT NULL
+    
+);
+
+CREATE TABLE Autores(
+	id_autores INT AUTO_INCREMENT PRIMARY KEY,
+    aut_nome VARCHAR(75),
+    aut_nacionalidade VARCHAR(20),
+    aut_biografia VARCHAR(50)
+);
+
+CREATE TABLE Estoque(
+	id_livro INT NOT NULL,
+    est_entrada DATE NOT NULL,
+    est_saida DATE NOT NULL
+);
+
+ALTER TABLE Estoque
+ADD CONSTRAINT fk_id_livro FOREIGN KEY (id_livro) REFERENCES Livros(id_livro);
